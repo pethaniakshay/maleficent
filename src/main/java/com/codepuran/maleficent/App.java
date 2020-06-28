f78e469d4c52537f9b8dcb09b6e5cacbb6633828
Akshay Pethani <patelaksh412@gmail.com>
2020-06-28 23:46:20 +0530

    Initial Commit

---
+++ src/main/java/com/codepuran/maleficent/App.java
@@ -0,0 +1,9 @@
+package com.codepuran.maleficent;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/codepuran/maleficent/AppTest.java
@@ -0,0 +1,13 @@
+package com.codepuran.maleficent;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue( true );
+    }
+}
