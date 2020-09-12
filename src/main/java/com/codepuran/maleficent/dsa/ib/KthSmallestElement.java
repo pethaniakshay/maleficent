c0f6d7de3f6fd0780a80b692b2c1858396bd3430
Akshay Pethani <patelaksh412@gmail.com>
2020-09-12 21:24:34 +0530

    Linked List - Practice

--- src/main/java/com/codepuran/maleficent/dsa/ib/KthSmallestElement.java
+++ src/main/java/com/codepuran/maleficent/dsa/ib/KthSmallestElement.java
@@ -1,8 +1,38 @@
 package com.codepuran.maleficent.dsa.ib;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class KthSmallestElement {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        List<Integer> A = Arrays.asList(1,2,13,6,8,5,7);
+        int B = 2;
+        System.out.println("Answer: " + kthsmallest(A,B));
+    }
+
+    public static int kthsmallest(final List<Integer> A, int B) {
+        int k = 1;
+        int smallestIntPosition = 0;
+        int possibleSmallestIntPosition = -1;
+        int i = 0;
+        for(int element : A) {
+            if(element < A.get(smallestIntPosition)) {
+                smallestIntPosition = i;
+            }
+            ++i;
+        }
+        while (k < B) {
+            for(int j = 0; j < A.size() ; j++ ) {
+                if(j!= smallestIntPosition && (A.get(j) >= A.get(smallestIntPosition)) && (possibleSmallestIntPosition == -1 || A.get(j) <= A.get(possibleSmallestIntPosition))) {
+                    possibleSmallestIntPosition = j;
+                }
+            }
+            if(possibleSmallestIntPosition != -1)
+                smallestIntPosition = possibleSmallestIntPosition;
+            possibleSmallestIntPosition = -1;
+            ++k;
+        }
+        return A.get(smallestIntPosition);
     }
 }
---
+++ src/main/java/com/codepuran/maleficent/dsa/ib/ll/CustomLinkedList.java
@@ -0,0 +1,49 @@
+package com.codepuran.maleficent.dsa.ib.ll;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CustomLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+    }
+
+    /*public ListNode reverseList(ListNode A) {
+        if (A == null || A.next == null) {
+            return A;
+        }
+        ListNode prev = A;
+        ListNode current = A.next;
+        prev.next = null;
+        ListNode third;
+        while (current.next != null) {
+            third = current.next;
+            current.next = prev;
+            prev = current;
+            current = third;
+        }
+        current.next = prev;
+        return current;
+    }*/
+
+    public ListNode reverseList(ListNode A) {
+        if(A == null) {
+            return A;
+        }
+        if(A.next == null) {
+            return A;
+        }
+        ListNode newHeadNode = reverseList(A.next);
+        A.next.next = A;
+        A.next = null;
+        return newHeadNode;
+    }
+}
+
+class ListNode {
+      public int val;
+      public ListNode next;
+ ListNode(int x) { val = x; next = null; }
+ }
